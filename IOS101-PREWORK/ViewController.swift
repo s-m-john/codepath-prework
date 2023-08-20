5cee08ddba4cfea33a428f9650740610ac0075d5
Sophia John <smc_john@hotmail.com>
2023-08-20 02:57:22 -0400

    Initial Commit

---
+++ IOS101-PREWORK/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOS101-PREWORK
+//
+//  Created by Sophia  on 8/20/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
